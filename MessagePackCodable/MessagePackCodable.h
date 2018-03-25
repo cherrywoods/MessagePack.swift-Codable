3025ed5c9ad0cfe05b79d6b103249a3f3f9caadc
cherrywoods <cherrywoods@posteo.org>
2018-03-25 16:12:56 +0200

    Imported dependencies using Carthage, started implementing

---
+++ MessagePackCodable/MessagePackCodable.h
@@ -0,0 +1,20 @@
+//
+//  MessagePackCodable_iOS.h
+//  MessagePackCodable iOS
+//  
+//  Available at the terms of the LICENSE file included in this project.
+//  If none is included, available at the terms of the unlicense. 
+//  See https://www.unlicense.org
+// 
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MessagePackCodable.
+FOUNDATION_EXPORT double MessagePackCodable_VersionNumber;
+
+//! Project version string for MessagePackCodable.
+FOUNDATION_EXPORT const unsigned char MessagePackCodable_VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MessagePackCodable_iOS/PublicHeader.h>
+
+
---
+++ Tests/MessagePackCodable_iOSTests.swift
@@ -0,0 +1,37 @@
+//
+//  MessagePackCodable_iOSTests.swift
+//  MessagePackCodable iOSTests
+//  
+//  Available at the terms of the LICENSE file included in this project.
+//  If none is included, available at the terms of the unlicense. 
+//  See https://www.unlicense.org
+// 
+
+import XCTest
+@testable import MessagePackCodable_iOS
+
+class MessagePackCodable_iOSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
